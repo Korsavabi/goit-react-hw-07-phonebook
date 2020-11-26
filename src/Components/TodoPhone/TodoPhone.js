a12a1210c3a8885791a1f5b6facba87d8168b4d9
Bogdan Tkachenko <61245273+Korsavabi@users.noreply.github.com>
2020-11-26 14:20:16 +0200

    HomeWork-07

--- src/Components/TodoPhone/TodoPhone.js
+++ src/Components/TodoPhone/TodoPhone.js
@@ -63,15 +63,16 @@ const TodoPhone = () => {
 			</Title>
 			<Form addTask={addTask} />
 			{<ERROR>{errorReducer}</ERROR>}
-			{loader && <Loader type="Puff"
+			{ loader && <Loader type="Puff"
 				color="#00BFFF"
 				height={100}
 				width={100}
-				timeout={3000} /> &&
+				timeout={3000} />}
+			{!errorReducer && !loader && (
 				<Section title={"Contacts:"}>
 					{contacts.length > 1 && <FilterForm />}
 					<PhoneItem deleteTask={deleteTask} contacts={contacts} />
-				</Section>}
+				</Section>)}
 		</>
 	)
 }
